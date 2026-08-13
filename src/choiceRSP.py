dbbeebab168d1eece24c68d92a8a12aea77dd802
sourpurin <59275057+iyuri2519@users.noreply.github.com>
2026-08-13 10:13:50 +0900

    하나빼기 ver1.3
    is_game_continue 제거
    RunRSP 데이터 검증 순서 수정 (길이부터 확인)
    index => indices
    list[int,int] => list[int]
    함수명을 스네이크 형식으로 표현

--- src/choiceRSP.py
+++ src/choiceRSP.py
@@ -1,24 +1,15 @@
 import random as rd
 
-# 3판 2선승제 게임에서 종료할 상황인지 판단합니다.
-# True : 계속, False : 종료
-def is_game_continue(player_a:int, player_b:int) -> bool:
-    if (player_a + player_b) < 3 and player_a < 2 and player_b < 2:
-        return True
-    return False
-
 # 가위바위보 함수를 시행하고, 플레이어가 선택한 두개의 index를 반환합니다.
-def RunRSP(arr:list) -> list[int, int]:
+def run_rsp(arr:list) -> list[int]:
     while True:
         player = input("플레이어 : ").strip().split()
-        if player == []:
-            print("말을 하세요!")
+        if len(player) != 2:
+            print("두 단어를 입력해주세요!")
             continue
 
         s1, s2 = player[0], player[-1]
-        if len(player) != 2:
-            print(f"두 단어를 입력해주세요!", end="")
-        elif s1 == s2:
+        if s1 == s2:
             print("다른 걸 내주세요!", end="")
         elif s1 not in arr or s2 not in arr:
             print("가위, 바위, 보 중에 내주세요!", end="")
@@ -27,8 +18,8 @@ def RunRSP(arr:list) -> list[int, int]:
         print(f" Ex) {arr[0]} {arr[1]}")
 
 # 하나빼기 함수를 시행하고, 플레이어가 선택한 것의 index를 반환합니다.
-def ChoiceRSP(arr:list, player_idx:list) -> int:
-    player_arr = [arr[player_idx[0]], arr[player_idx[1]]]
+def choice_rsp(arr:list, player_indices:list) -> int:
+    player_arr = [arr[player_indices[0]], arr[player_indices[1]]]
     while True:
         player_choice = input("플레이어 선택 : ").strip()
         if player_choice not in player_arr:
@@ -43,7 +34,7 @@ def is_player_win(player_idx:int, other_idx:int) -> int:
         return 0
     elif player_idx == (other_idx+1) % 3:
         return 1
-    return -1               # player_idx < 0 인 경우도 포함
+    return -1
 
 
 RSP = ["가위", "바위", "보"]
@@ -53,15 +44,15 @@ print("무승부는 승부에 포함 안합니다.")
 player_win = 0
 com_win = 0
 
-while is_game_continue(player_win, com_win):
+while player_win < 2 and com_win < 2:
     print("\n가위 바위 보!")
-    player_idx = RunRSP(RSP)            # 가위바위보 (두개내기) 를 시행
-    com_idx = rd.sample([0, 1, 2], 2)   # SiRoPa의 index를 랜덤으로 두개 고름
-    print(f"컴퓨터 : {RSP[com_idx[0]]} {RSP[com_idx[1]]}")
+    player_indices = run_rsp(RSP)            # 가위바위보 (두개내기) 를 시행
+    com_indices = rd.sample([0, 1, 2], 2)   # SiRoPa의 index를 랜덤으로 두개 고름
+    print(f"컴퓨터 : {RSP[com_indices[0]]} {RSP[com_indices[1]]}")
 
     print("하나 빼기!")
-    player_choice_idx = ChoiceRSP(RSP, player_idx)  # 하나빼기를 시행
-    com_choice_idx = rd.choice(com_idx)         # com_idx에서 하나 고름
+    player_choice_idx = choice_rsp(RSP, player_indices)  # 하나빼기를 시행
+    com_choice_idx = rd.choice(com_indices)         # com_idx에서 하나 고름
     print(f"컴퓨터 선택 : {RSP[com_choice_idx]}")
     
     win_num = is_player_win(player_choice_idx, com_choice_idx)
